ac350a0635957b3c94750d7c8363f0daee3c59bc
Thiago Vieira Chioda <70536163+Chioda@users.noreply.github.com>
2022-04-11 21:41:58 -0300

    Update index.js

--- src/http/index.js
+++ src/http/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const http = axios.create({
-    baseURL: 'http://localhost:8000/',
+    baseURL: 'https://cond-viver.vercel.app/',
     headers: {
         'Accept': 'application/json',
         'Content': 'application/json'
