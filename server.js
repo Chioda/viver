492aa532c5bacae1ef34ccf8b8f862584e17daed
Thiago Vieira Chioda <thiagochioda@hotmail.com>
2022-04-14 22:17:31 -0300

    heroku

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
-const fs = require('fs')
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
+var app = express()
+app.use(serveStatic(path.join(__dirname,'dist')))
+var port = process.env.PORT || 5000
+app.listen(port)
+console.log('server started ' + port)
+
+
+const fs = require('fs')  
 const bodyParser = require('body-parser')
 const jsonServer = require('json-server')
 const jwt = require('jsonwebtoken')
